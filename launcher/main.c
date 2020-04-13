96616105f6afc8f0196668309b2b18e17dd670b1
TheFloW <theofficialflow1996@gmail.com>
2020-04-13 20:10:01 +0200

    Implement kermit handshake

--- launcher/main.c
+++ launcher/main.c
@@ -1,5 +1,6 @@
 #include <psp2/compat.h>
 #include <psp2/ctrl.h>
+#include <psp2/display.h>
 #include <psp2/io/dirent.h>
 #include <psp2/io/fcntl.h>
 
@@ -37,7 +38,7 @@ int main() {
   res = sceCompatAllocCdramWithHole(&cdram);
   printf("sceCompatAllocCdramWithHole: %x\n", res);
 
-  res = sceCompatInitEx(0);
+  res = sceCompatInitEx(1);
   printf("sceCompatInitEx: %x\n", res);
 
   memcpy(cdram.uncached_cdram + 0x600000,
@@ -56,16 +57,29 @@ int main() {
   res = sceCompatStart();
   printf("sceCompatStart: %x\n", res);
 
+  res = sceCompatWaitSpecialRequest(1);
+  printf("sceCompatWaitSpecialRequest: %x\n", res);
+
   while (1) {
-    psvDebugScreenSetXY(0, 5);
+    psvDebugScreenSetXY(0, 6);
+    printf("Press X to raise interrupt.\n");
     printf("Press START to exit.\n");
-    printf("Value: %x\n", *(int *)cdram.cached_cdram);
+    printf("\n");
+    printf("Counter: %x\n", *(int *)cdram.cached_cdram);
+    printf("Interrupt bits: %x\n", *(int *)(cdram.cached_cdram + 4));
+    printf("\n");
 
     SceCtrlData pad;
     sceCtrlReadBufferPositive(0, &pad, 1);
+    if (pad.buttons & SCE_CTRL_CROSS) {
+      res = sceCompatInterrupt(1);
+      printf("sceCompatInterrupt: %x\n", res);
+    }
     if (pad.buttons & SCE_CTRL_START) {
       break;
     }
+    
+    sceDisplayWaitVblankStart();
   }
 
   sceCompatStop();
